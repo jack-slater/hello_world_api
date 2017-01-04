459da45b7635c116cfa0957b33f108a32678a240
Natalia <natalia@Natalias-MacBook-Pro.local>
2017-01-04 18:02:26 +0000

    Change port to 5000

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/hello', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 4000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!');
 });
