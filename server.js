8fa50f1931143cde69180fa8eaf93b35c209ece6
slaterJJ <jackslater@hotmail.com>
2017-01-04 16:06:21 +0000

    Add process.env port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use('/', function (req, res) {
   res.send('Hello Earth')
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
