36ddfec51c6643a0be74cd1080d6bd7b8746a605
slaterJJ <jackslater@hotmail.com>
2017-01-04 17:52:13 +0000

    Change port on server

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/hello', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 4000, function () {
+  console.log('Example app listening on port 3000!');
 });
